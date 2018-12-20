845a9189b4641ea9420dac1472b224fb2bfe8e34
Oscar canton <oscar@iMac-de-Oscar.local>
2018-12-20 11:12:24 +0100

    establishing the connection with Mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+'use strict';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -13,19 +14,29 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//getting mongooseConnection
+require('./lib/mongooseConnection');
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/**
+ * Web Aplication routes
+ */
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
